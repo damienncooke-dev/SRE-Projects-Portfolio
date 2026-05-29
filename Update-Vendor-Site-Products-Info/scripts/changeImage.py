d561de894df5c4be9aa4e61fdcf7030cedffecc3
Damien Cooke <damienncooke@gmail.com>
2026-05-29 06:40:10 -0400

    Added code to: -Import modules: Image, Path, os -Set directory paths -Created a list of image files -Iterate through the list of images, resize them and save them in 'jpeg' format

--- Update-Vendor-Site-Products-Info/scripts/changeImage.py
+++ Update-Vendor-Site-Products-Info/scripts/changeImage.py
@@ -7,6 +7,20 @@ Works on the image files with '.tiff' extension to resize them from 3000x2000 to
 
 from PIL import Image
 from pathlib import Path
+import os
 
+# Set directory path objects
 main_dir = Path(__file__).parent.parent
+image_dir = main_dir / "supplier-data" / "images"
+
+# Creates a list of all the image files in the directory
+images = [file.name for file in image_dir.iterdir()]
+
+# Iterate through the list of images, split filename and extension. Confirm only '.tiff' files will be processed.
+for file in images:
+   fname, ext = os.path.splitext(file) # Get filename, extension
+   if "tiff" in ext:  # Check if extension is .tiff
+      file_img = Image.open(image_dir / file) # Creates image object
+      file_img.resize((600,400)).convert("RGB").save(image_dir / f"{fname}.jpeg")  # Resize, convert to RGB and save as jpeg
+
 
