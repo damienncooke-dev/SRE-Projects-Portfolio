510f3ae8526be6c455ad64ae873116c8fa7e2e00
Damien Cooke <damienncooke@gmail.com>
2026-05-29 19:25:29 -0400

    Added code to: - Imported reportlab package to access methods: SimpleDocTemplate, "flowables", and style sheet - Defined method to generate PDF report

--- Update-Vendor-Site-Products-Info/scripts/reports.py
+++ Update-Vendor-Site-Products-Info/scripts/reports.py
@@ -4,14 +4,17 @@
 Module to define the methods to generate a PDF report
 
 """
-
-import os
-from pathlib import Path
 from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib import colors
 
-main_dir = Path(__file__).parent.parent
+# Define method generate_report() to create PDF report using output 'filename', pdf 'title' and product_data
 
-def generate_report():
+def generate_report(filename, title, paragraph):
+   styles = getSampleStyleSheet()
+   report = SimpleDocTemplate(filename) # pdf filename to write to
+   report_title = Paragraph(title, styles["h1"])
+   report_info = Paragraph(paragraph, styles["BodyText"])
+   empty_line = Spacer(1,20)
+   report.build([report_title, empty_line, report_info])
