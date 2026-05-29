3229178eafeae060ec8545f5af0fa2e973951946
Damien Cooke <damienncooke@gmail.com>
2026-05-29 06:56:22 -0400

    Added code to get file with vendor information from url

--- Update-Vendor-Site-Products-Info/scripts/download_vendor_data.sh
+++ Update-Vendor-Site-Products-Info/scripts/download_vendor_data.sh
@@ -1,4 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Copy vendor file called: 'supplier-data.tar.gz'
+# Copy vendor file called: 'supplier-data.tar.gz'.
+# Note: 'wget' does not work on MacOS by default. Use 'curl -O' to get file from URL and save.
+curl -O https://storage.googleapis.com/gwg-content/gic223/supplier-data.tar.gz
+
+# Use 'tar -xf <file>' to extract
+# Need update to script to ask for confirmation to extract file
+
 
