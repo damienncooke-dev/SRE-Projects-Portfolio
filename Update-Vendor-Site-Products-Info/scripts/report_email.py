2adea9a97e79ccd81ccf4e889a4e470ce14bc489
Damien Cooke <damienncooke@gmail.com>
2026-05-29 20:02:36 -0400

    Added code to: - Import module reports that has method to generate PDF report - Import module emails that has methods to generate and send email - Set directory paths - Get list of files containing the description of the products - Iterate over the list of files to create a list of product information in JSON format - Created attributes and called method for generating PDF - Created attributes and called methods for generating and sending email

--- Update-Vendor-Site-Products-Info/scripts/report_email.py
+++ Update-Vendor-Site-Products-Info/scripts/report_email.py
@@ -5,11 +5,47 @@ Processes the description data generated from the execution of the run.py. Calls
 
 """
 
-import os
-import datetime
+import datetime as df
 import reports
-import email
+import emails
 from pathlib import Path
 
-main_dir = Path(__file__).parent.parent
+
+# Define a main() function that will do the file processing and generate PDF
+def main():
+    # Set the directory path objects
+    main_dir = Path(__file__).parent.parent
+    description_dir = main_dir / "supplier-data" / "descriptions"
+
+    # Get the list of files in the directory to iterate over
+    desc_text = [file.name for file in description_dir.iterdir()]
+
+    desc_text_list = []  # Create an empty list to append description information read from each text file
+
+    # Iterate over the list of files
+    for file in desc_text:
+        description_file = description_dir / file
+        with open(description_file, 'r') as f:
+            desc_text_list.append({"name": f.readline().rstrip("\n"),
+                                   "weight": f.readline().rstrip("\n")})
+
+    # Create title, filename path, and paragraph for the PDF Document
+    title = f"Processed Update on {df.datetime.now().strftime('%b-%d-%Y')}"
+    filename = str(main_dir / "outfiles" / "processed.pdf")
+    paragraph = ""  # create empty paragraph string for accumulation of description information
+    for record in sorted(desc_text_list, key=lambda x: x['name']):  # sort the data using the key 'name'
+        paragraph +=f"name: {record['name']}<br/>weight: {record['weight']}<br/><br/>"
+
+    # Create the PDF report
+    reports.generate_report(filename, title, paragraph)
+    return filename
+
+
+if __name__ == "__main__":
+    attachments = main()
+    print(attachments)
+    subject = "Upload Completed - Online Fruit Store"
+    body = "All fruits are uploaded to our website successfully. A detailed list is attached to this email."
+    msg = emails.generate_email(subject, body, attachments)
+    emails.send_email(msg)
 
