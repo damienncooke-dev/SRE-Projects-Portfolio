56b8a4030b8ea52d962059325b99a2eb20e1994a
Damien Cooke <damienncooke@gmail.com>
2026-05-30 00:37:21 -0400

    Added code to: - Import shutil, psutil, socket, emails - Check disk usage < 20% - Check CPU usage > 80% - Checks for available memory < 100mb - Checks hostname and if it resolves to "127.0.0.1"

--- Update-Vendor-Site-Products-Info/scripts/health_check.py
+++ Update-Vendor-Site-Products-Info/scripts/health_check.py
@@ -1,16 +1,47 @@
 #!/usr/bin/env python3
 
 """
-Script used to check the health of the server every 60 seconds. If an incident is detected, an email is sent alerting to the error condition
+Script used to demonstrate various systems checks every 60 seconds enabled in cron. If an incident is detected, an email is sent alerting to the error condition
 
 """
 
-import os
 import shutil
-from pathlib import Path
-
 import psutil
+import socket
+import emails
+import os
 
 
-main-dir = Path(__file__).parent.parent
+body = "Please check your system and resolve the issue as soon as possible."
+
+# Checks disk usage and sends email if available space < 20%
+du = shutil.disk_usage("/")
+du_free = du.free/du.total * 100  # Calculate free space as a percentage
+if du_free < 20:
+    subject = "Error - Available disk space is less than 20%"
+    message = emails.generate_error_email(subject, body)
+    emails.send_email(message)
+
+# Checks CPU usage and sends email if usage >80%
+cpu_util = psutil.cpu_percent(1)
+if cpu_util > 80:
+    subject = "Error - CPU usage is over 80%"
+    message = emails.generate_error_email(subject, body)
+    emails.send_email(message)
+
+# Checks for available memory, if < 100mb sends an email
+mem = psutil.virtual_memory()
+target = 100 * 1024 * 1024  # (100 * 1024 KB * 1024 Bytes)
+if mem.available < target:
+    subject = "Error - Available memory is less than 100MB"
+    message = emails.generate_error_email(subject, body)
+    emails.send_email(message)
+
+# Checks hostname and if cannot be resolved to "127.0.0.1" sends an email
+hostname = socket.gethostbyname('localhost')
+if hostname != '127.0.0.1':
+    subject = "Error - localhost cannot be resolved to 127.0.0.1"
+    message = emails.generate_error_email(subject, body)
+    emails.send_email(message)
+
 
