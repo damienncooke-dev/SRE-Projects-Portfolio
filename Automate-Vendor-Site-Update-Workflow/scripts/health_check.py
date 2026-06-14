08ba4dfe58b321e9eacdd512626c058616208ea4
Damien Cooke <damienncooke@gmail.com>
2026-06-14 16:17:12 -0400

    Updated description of the check hostname code block to be more descriptive

--- Automate-Vendor-Site-Update-Workflow/scripts/health_check.py
+++ Automate-Vendor-Site-Update-Workflow/scripts/health_check.py
@@ -36,7 +36,7 @@ if mem.available < target:
     message = emails.generate_error_email(subject, body)
     emails.send_email(message)
 
-# Checks hostname and if cannot be resolved to "127.0.0.1" sends an email
+# Checks internal network adapter and TCP/IP stack by trying to resolve hostname to "127.0.0.1" and sends an email if it can't
 hostname = socket.gethostbyname('localhost')
 if hostname != '127.0.0.1':
     subject = "Error - localhost cannot be resolved to 127.0.0.1"
