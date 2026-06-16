16ed00048cd08e3aa6dc3a84e4616a4090bcbf0b
Damien Cooke <damienncooke@gmail.com>
2026-06-16 13:07:09 -0400

    - Added environment variable FRUIT_COMP_URL to extract url information for company website - Added import of dotenv to load environment variables - Modified url variable to build url with loaded env variable - Added print statement to state where the files were uploaded to and for checking site for the upload

--- Automate-Workflow-Vendor-Site-Update/scripts/supplier_image_upload.py
+++ Automate-Workflow-Vendor-Site-Update/scripts/supplier_image_upload.py
@@ -8,23 +8,34 @@ Uploads images with '.jpeg' extensions to the website
 import requests
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
 
 # Set directory path objects
 main_dir = Path(__file__).parent.parent
 image_dir = main_dir / "supplier-data" / "images"
+dotenv_path = main_dir / ".dotenv"
+
+# Load environment variables from .env file
+load_dotenv(dotenv_path=dotenv_path)
+fruit_store_url = os.getenv("FRUIT_COMP_URL")
+
 
 # Iterate over image_dir and get list of filenames
 images = [file.name for file in image_dir.iterdir()]
 
-url = "http://34.69.110.154/upload/"  # URL to upload images to
+url = f"http://{fruit_store_url}/upload/"  # URL to upload images to
 
-for file in images:   # Iterate over list of image filenames
-   fname, ext = os.path.splitext(file)  # Get filename, extension
-   image_file = image_dir / file  # Get full path to image file
-   if ".jpeg" in ext:  # Only upload .jpeg files
-      with open (image_file, 'rb') as opened:  # Open image file in binary mode
-         result = requests.post(url, files={'file': opened})  # POST request to upload image
+# for file in images:   # Iterate over list of image filenames
+#    fname, ext = os.path.splitext(file)  # Get filename, extension
+#    image_file = image_dir / file  # Get full path to image file
+#    if ".jpeg" in ext:  # Only upload .jpeg files
+#       with open (image_file, 'rb') as opened:  # Open image file in binary mode
+#          result = requests.post(url, files={'file': opened})  # POST request to upload image
+
+print("Uploaded images to: {}".format(url))
+print("Check the website for the uploaded images.")
 
 # Add error handling for non-connection
-# Add check to prevent duplicate uploads, maybe tag the files as uploaded is POST request is successful.
+# Add check to prevent duplicate uploads, maybe tag the files as uploaded if POST request is successful.
 
